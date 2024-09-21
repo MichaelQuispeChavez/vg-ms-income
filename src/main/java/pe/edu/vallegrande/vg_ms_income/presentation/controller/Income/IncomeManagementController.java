232c3935a042c3c3e3b303540916b30247e7d393
Jhanmarco Godoy Levano <111781171+JhanmarcoGodoyLevano@users.noreply.github.com>
2024-09-21 15:34:21 -0500

    v11

--- src/main/java/pe/edu/vallegrande/vg_ms_income/presentation/controller/Income/IncomeManagementController.java
+++ src/main/java/pe/edu/vallegrande/vg_ms_income/presentation/controller/Income/IncomeManagementController.java
@@ -9,7 +9,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin
 @RequestMapping("management/income/v1")
 public class IncomeManagementController {
     private final IncomeService incomeService;
